701f3e1079588f5913db039047b91ee1b0e7e633
Joy Ayitey <jijakahn6@gmail.com>
2019-11-09 13:10:50 +0000

    Procfile for heroku

--- config/app.php
+++ config/app.php
@@ -52,7 +52,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost/bcom'),
+    'url' => env('APP_URL', 'http://localhost/'),
 
     /*
     |--------------------------------------------------------------------------
